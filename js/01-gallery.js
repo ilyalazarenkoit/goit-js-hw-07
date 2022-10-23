2ed3849ec6631bc9618e535b04ef359608420d13
ilyalazarenkoit <ilyalazarenkoit@gmail.com>
2022-10-23 17:50:48 +0300

    first commit before check
    qwe

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+let gallery = document.querySelector(".gallery")
+let markup = galleryItems.map(item => 
+  `<div class="gallery__item"><a class="gallery__link" href=${item.original}>
+  <img
+    class="gallery__image"
+    src=${item.preview}
+    data-source=${item.original}
+    alt=${item.description}/></a></div>`
+).join("");
+
+gallery.innerHTML = markup;
+gallery.addEventListener("click", selectTarget)
+
+function selectTarget(event) {
+    event.preventDefault()
+    if(event.target.nodeName !== "IMG") {
+      return
+    }
+    const instance = basicLightbox.create(`<img
+    src=${event.target.dataset.source} width="800" height="600" />`)
+    instance.show()
+    gallery.addEventListener("keydown", event => {
+        if(event.code === "Escape") {
+        instance.close()
+        }
+    })
+}
+
